8ae0807f83a2826ca6b85be2ceeebaf435aca89b
DaniilTulkin <tulkindaniilal@gmail.com>
2022-07-17 16:15:54 +0300

    delete config

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,20 +1,81 @@
+const { validationResult } = require('express-validator')
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 
 module.exports.register = async function(req, res) {
     try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'Registration: validation error'
+            })
+        }
+
         const {email, password} = req.body
         const candidate = await User.findOne({email})
 
         if (candidate) {
             return res.status(400).json({
-                message: 'User is already exists'
+                message: 'Registration: user already exists'
             })
         }
 
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const user = new User({email, password: hashedPassword})
+        await user.save()
+
+        res.status(201).json({
+            message: 'Registration: user have been created'
+        })
     }
     catch (e) {
         res.status(500).json({
-            message: 'Registeration error'
+            message: 'Registration: internal server error'
+        })
+    }
+}
+
+module.exports.login = async function(req, res) {
+    try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'Registration: validation error'
+            })
+        }
+
+        const {email, password} = req.body
+        const user = await User.findOne({email})
+        if (!user) {
+            return res.status(400).json({
+                message: 'Login: user not exists'
+            })
+        }
+
+        const isMatch = await bcrypt.compare.compare(password, user.password)
+        if (!isMatch) {
+            return res.status(400).json({
+                message: 'Login: password is not correct'
+            })
+        }
+
+        const token = jwt.sign(
+            { userId: user.id },
+            config.get('jwtSecret'),
+            { expiresIn: '30d'}
+        )
+        res.json({
+            token, 
+            userId: user.id
+        }) 
+    }
+    catch (e) {
+        res.status(500).json({
+            message: 'Login: internal server error'
         })
     }
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,13 +1,23 @@
 const { Router } = require('express')
+const { check } = require('express-validator')
 
 const controller = require('../controllers/auth.controller')
 
 const router = Router()
 
-router.post('/register', controller.login)
-
-router.post('/login', async (req, res) => {
-
-})
+router.post(
+    '/register',
+    [
+        check('email', 'Email is not correct').isEmail(),
+        check('password', 'Password is not correct').isLength({min: 6})
+    ],
+    controller.register)
+router.post(
+    '/login', 
+    [
+        check('email', 'Email is not correct').isEmail(),
+        check('password', 'Password is not correct').isLength({min: 6})
+    ],
+    controller.login)
 
 module.exports = router
